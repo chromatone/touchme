46d901b952a8c092d2b5c63ac0ac15fe3fb5e53d
davay <fond@fondfrukt.ru>
2022-04-07 23:14:39 +0300

    fix

--- src/components/scene/donut.vue
+++ src/components/scene/donut.vue
@@ -34,14 +34,14 @@ g.donut
       style="transition: all 500ms ease-out"
       :cx="width / 2", 
       :cy="height / 2", 
-      :r="width / 4", 
+      :r="(height < width ? height / 4 : width / 4)", 
       fill="transparent", 
       :stroke="pitchColor(index)", 
       :stroke-width="width / 4"
       :stroke-dasharray="circ"
       :stroke-dashoffset="getOffset(value)"
-
-      :transform="`rotate(${chart[index] || 0}, ${width / 2},${height / 2})`"
+      :transform-origin="`${width / 2} ${height / 2}`"
+      :transform="`rotate(${chart[index] || 0})`"
       )
   circle(
     style="transition: all 200ms ease-out"
@@ -49,8 +49,14 @@ g.donut
     :cy="height / 2"
     :r="30"
     opacity="0.9"
-    :transform="`scale(${midi?.note?.attack * midi.duration / 500 + 1})`"
+    :transform="`scale(${midi?.note?.attack * midi.duration / 500 + 1 || 1})`"
     :transform-origin="`${width / 2} ${height / 2}`"
     :fill="pitchColor(midi?.note?.pitch, 3, midi?.note?.attack)"
   )
+  text(
+    :x="width / 2"
+    :y="height / 2 + 8"
+    text-anchor="middle"
+    font-size="24"
+  ) {{ midi?.note?.identifier }}
 </template>
