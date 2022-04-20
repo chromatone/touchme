73b4992cd1cdb19c17e6c89c19557cf2979161db
davay <fond@fondfrukt.ru>
2022-04-21 00:53:09 +0300

    fix

--- src/components/stats/panel.vue
+++ src/components/stats/panel.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { enter, server, useUser, useUserSession, useUserSessions, formatDate, useUsers, useSessions } from '~/use/stats';
 import { userColor } from '~/use/colors';
-import { useTimeAgo } from '@vueuse/core';
-import { computed } from 'vue';
+import { onClickOutside, useTimeAgo } from '@vueuse/core';
+import { computed, ref } from 'vue';
 import ms from 'ms';
 
 const user = useUser()
@@ -19,10 +19,14 @@ const users = useUsers()
 
 const { sessionCount, totalTime, avgTime } = useSessions()
 
+const target = ref()
+
+onClickOutside(target, () => server.open = false)
+
 </script>
 
 <template lang='pug'>
-.flex.flex-col.overflow-hidden.rounded-lg.shadow-xl.z-1000.absolute.bg-light-100.dark_bg-dark-400.left-20.right-20.top-20(v-if="server.open")
+.flex.flex-col.overflow-hidden.rounded-lg.shadow-xl.z-1000.absolute.bg-light-100.dark_bg-dark-400.left-20.right-20.top-20(v-if="server.open" ref="target")
   .p-2.text-xs(v-if="user?.id" :style="{ backgroundColor: userColor.hex(user?.email) }") {{ user?.email }}
   .p-4.flex.flex-col.gap-2
     .text-xl STATISTICS
@@ -32,7 +36,7 @@ const { sessionCount, totalTime, avgTime } = useSessions()
       p Total time: {{ ms(totalTime || 0) }} 
       p Average session time: {{ ms(avgTime || 0) }}
     .p-0 First started {{ age.value }}
-    .text-md Current session: {{ ms(session.pulse - session.started || 0) }}
+    .text-md Current session: {{ ms(session?.pulse - session?.started || 0) }}
     .font-bold My sessions
     .flex.flex-col.gap-1.max-h-50.overflow-y-scroll.overscroll-y-contain
       .py-1.px-2.flex.items-center.gap-2.bg-light-800.dark_bg-dark-50.rounded(v-for="s in sessions" :key="s")
