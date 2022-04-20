c23188b698c28bcb8c1900b23e62524ebb40d610
davay <fond@fondfrukt.ru>
2022-04-20 16:59:42 +0300

    stats server communication

--- src/components/stats/button.vue
+++ src/components/stats/button.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useStorage } from '@vueuse/core';
 import { computed, reactive, ref } from 'vue';
+import { nanoid } from 'nanoid'
 
 const updateRequest = {
   method: 'PUT',
@@ -8,34 +9,59 @@ const updateRequest = {
   body: JSON.stringify({ title: 'Fetch PUT Request Example' })
 };
 const server = reactive({
-  url: 'http://localhost:2242/api',
+  // url: 'https://powerful-scented-galley.glitch.me',
+  url: 'http://localhost:2242',
   response: null
 })
 
 const stat = ref({})
 const user = reactive({
-  name: useStorage('user-name', ''),
-  email: useStorage('user-email', ''),
-  jwt: '',
+  nid: useStorage('nano-id', ''),
+  accessToken: '',
   authedAt: 0,
+  exists: false,
+  profile: {}
 })
 
 const interact = async () => {
-  if (!user.jwt && Date.now() - user.authedAt > 2000) {
-    server.response = await fetch(`${server.url}/register`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        "email": "olivier@mail.com",
-        "password": "bestPassw0rd"
-      })
-    })
+  if (!user.accessToken && Date.now() - user.authedAt > 2000) {
+    generateUser()
+  } else {
+    console.log(user.accessToken)
   }
   //gets current user stats
   // fetch('http://localhost:2242/api/db')
   //   .then(res => res.json())
   //   .then(data => stat.value = data)
   return stat
+}
+
+async function generateUser() {
+  user.nid = user.nid || nanoid()
+  let res = await fetch(`${server.url}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      "email": user.nid + '@touch.me',
+      "password": user.nid,
+      "name": user.nid,
+    })
+  })
+    .then(r => r.ok ? r : Promise.reject(r))
+    .then(r => r.json())
+    .catch(e => {
+      console.log(e)
+      if (e.status == 400) {
+        user.exists = true
+        console.log(user)
+      }
+    })
+  if (res?.accessToken) {
+    user.accessToken = res?.accessToken
+    user.profile = res?.user
+    user.authedAt = Date.now()
+  }
+
 }
 </script>
 
